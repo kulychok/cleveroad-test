a91697ba873a2542eda2f2bec633409db322fd17
kulychok <s.v.kulyk55@gmail.com>
2021-03-10 16:35:50 +0200

    added user registration

--- routes.js
+++ routes.js
@@ -51,4 +51,49 @@ router.post('/login', async (request, response) => {
 	}
 });
 
+router.post('/register', async (request, response) => {
+	try {
+		let { email, password, name, phone } = request.body;
+
+		const chekEmail = /.+@.+\..+/i;
+		if (!chekEmail.test(email)) {
+			response
+				.status(422)
+				.json([{ field: email, message: 'Wrong current email' }]);
+			return;
+		}
+
+		const chekPassword = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}/g;
+		if (!chekPassword.test(password)) {
+			response
+				.status(422)
+				.json([{ field: password, message: 'Wrong current password' }]);
+			return;
+		}
+
+		const chekPhone = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
+		if (phone && !chekPhone.test(phone)) {
+			response
+				.status(422)
+				.json([{ field: phone, message: 'Wrong current phone' }]);
+			return;
+		}
+
+		const token = jwt.sign({ email }, config.jwtSecret, {
+			expiresIn: '1d',
+		});
+
+		if (!phone) phone = 'Null';
+		const query = `INSERT INTO users (email, password, token, phone, name) VALUES("${email}", "${password}", "${token}", "${phone}", "${name}")`;
+
+		connection.query(query, (error) => {
+			if (error) {
+				response.status(422).json(error);
+			} else response.status(200).json({ token: token });
+		});
+	} catch (error) {
+		response.status(500).json({ message: error.message });
+	}
+});
+
 module.exports = router;
